b31293f9cc3801c5766ddb56ac4213f444f0dd81
OguzhanGrey <oguz271402@gmail.com>
2025-04-30 05:08:49 +0300

    lazy installed

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
-    rollupOptions: {
-      preserveEntrySignatures: "strict",
-    },
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json"],
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
   },
 });
