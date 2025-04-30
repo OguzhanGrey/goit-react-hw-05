948c9456457ef977d0e9743c73a99e0181bd2f83
OguzhanGrey <oguz271402@gmail.com>
2025-04-30 05:04:01 +0300

    lazy uninstalled

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
 import { RotateLoader } from "react-spinners";
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
-const MovieDetailsPage = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage.jsx")
-);
-const NotFoundPage = lazy(() =>
-  import("./pages/NotFoundPage/NotFoundPage.jsx")
-);
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast.jsx"));
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews.jsx")
-);
+import HomePage from "./pages/HomePage/HomePage";
+import MoviesPage from "./pages/MoviesPage/MoviesPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import MovieCast from "./components/MovieCast/MovieCast";
+import MovieReviews from "./components/MovieReviews/MovieReviews";
 
 function App() {
   return (
