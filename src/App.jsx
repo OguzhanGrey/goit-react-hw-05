bf8bc0ef3ea44e1c37c367cc94cd543494210623
OguzhanGrey <oguz271402@gmail.com>
2025-04-30 04:55:06 +0300

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
 import { RotateLoader } from "react-spinners";
 
-const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
 const MovieDetailsPage = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage")
+  import("./pages/MovieDetailsPage/MovieDetailsPage.jsx")
 );
-const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const NotFoundPage = lazy(() =>
+  import("./pages/NotFoundPage/NotFoundPage.jsx")
+);
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast.jsx"));
 const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
+  import("./components/MovieReviews/MovieReviews.jsx")
 );
 
 function App() {
